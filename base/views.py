bda8350b8ec5901fae1b0042e5fe6f66a3b5f7f1
akash121kumar <akashkumar121.ak.ak@gmail.com>
2021-10-19 10:30:19 +0530

    Search functionality added

--- base/views.py
+++ base/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import redirect, render
-from django.http import HttpResponse
-from .models import Room
+from django.db.models import Q
+from .models import Room,Topic
 from .form import RoomCreationForm
 
 def home(request):
-    rooms=Room.objects.all()
-    context={'rooms':rooms}
+
+    q=request.GET.get('q')
+    if q:
+        rooms=Room.objects.filter(
+            Q(topic__name__icontains=q)|
+            Q(name__icontains=q)|
+            Q(description__icontains=q)|
+            Q(host__username__icontains=q)
+            )
+    else:
+        rooms=Room.objects.all()
+    rooms_count= rooms.count()
+    topics=Topic.objects.all()
+    context={'rooms':rooms,'topics':topics,'rooms_count':rooms_count}
     
     return render(request,'base/home.html',context)
 
